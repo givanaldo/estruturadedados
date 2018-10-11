eadd886a9961ca1b76dc1a5689e6d9ea79b72f0d
Givanaldo Rocha <givanaldo@gmail.com>
2018-10-11 11:28:40 -0300

    Algoritmos de Ordenação

---
+++ Ordenação/src/ordenacao/QuickSort2.java
@@ -0,0 +1,33 @@
+package ordenacao;
+
+public class QuickSort2 {
+
+    public static void quicksort(int[] vetor, int inicio, int fim) {
+        if (inicio < fim) {
+            int posicaoPivo = partition(vetor, inicio, fim);
+            quicksort(vetor, inicio, posicaoPivo - 1);
+            quicksort(vetor, posicaoPivo + 1, fim);
+        }
+    }
+
+    private static int partition(int[] vetor, int inicio, int fim) {
+        int pivo = vetor[inicio];
+        int i = inicio + 1, f = fim;
+        while (i <= f) {
+            if (vetor[i] <= pivo) {
+                i++;
+            } else if (pivo < vetor[f]) {
+                f--;
+            } else {
+                int troca = vetor[i];
+                vetor[i] = vetor[f];
+                vetor[f] = troca;
+                i++;
+                f--;
+            }
+        }
+        vetor[inicio] = vetor[f];
+        vetor[f] = pivo;
+        return f;
+    }
+}
--- Ordenação/src/ordenacao/TesteDeCargaOrdenacaoComTempo.java
+++ Ordenação/src/ordenacao/TesteDeCargaOrdenacaoComTempo.java
@@ -6,7 +6,7 @@ import java.util.Collections;
 public class TesteDeCargaOrdenacaoComTempo {
 
     public static void main(String[] args) {
-        int numElementos = 100000;
+        int numElementos = 1000000;
         ArrayList<Integer> numeros = new ArrayList<>();
 
         // sequência aleatória
@@ -18,7 +18,6 @@ public class TesteDeCargaOrdenacaoComTempo {
         // sequência invertida
         //for (int i = numElementos-1; i >= 0; i--)
         //	numeros.add(i);
-        
         int[] elementos = new int[numElementos];
         for (int i = 0; i < numElementos; i++) {
             elementos[i] = numeros.get(i);
@@ -29,21 +28,39 @@ public class TesteDeCargaOrdenacaoComTempo {
 
         vetor = elementos.clone();
         startTime = System.currentTimeMillis();
-        Sort.bubblesort(vetor);
+        QuickSort2.quicksort(vetor, 0, vetor.length-1);
         endTime = System.currentTimeMillis();
-        System.out.println("Bubblesort: " + (endTime - startTime) + " milisegundos");
+        System.out.println("Quicksort (pivô v[0]): " + (endTime - startTime));
 
         vetor = elementos.clone();
         startTime = System.currentTimeMillis();
-        Sort.selectionsort(vetor);
+        QuickSort.quicksort(vetor);
         endTime = System.currentTimeMillis();
-        System.out.println("Selectionsort: " + (endTime - startTime) + " milisegundos");
+        System.out.println("Quicksort (pivô v[n/2]): " + (endTime - startTime));
+
+        vetor = elementos.clone();
+        startTime = System.currentTimeMillis();
+        MergeSort.mergesort(vetor);
+        endTime = System.currentTimeMillis();
+        System.out.println("Mergesort: " + (endTime - startTime));
 
         vetor = elementos.clone();
         startTime = System.currentTimeMillis();
         Sort.insertionsort(vetor);
         endTime = System.currentTimeMillis();
         System.out.println("Insertionsort: " + (endTime - startTime) + " milisegundos");
+        
+        vetor = elementos.clone();
+        startTime = System.currentTimeMillis();
+        Sort.selectionsort(vetor);
+        endTime = System.currentTimeMillis();
+        System.out.println("Selectionsort: " + (endTime - startTime) + " milisegundos");
+        
+        vetor = elementos.clone();
+        startTime = System.currentTimeMillis();
+        Sort.bubblesort(vetor);
+        endTime = System.currentTimeMillis();
+        System.out.println("Bubblesort: " + (endTime - startTime) + " milisegundos");
 
     }
 }
