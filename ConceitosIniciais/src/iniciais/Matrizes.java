a29156c89b53d340b97d638f051b421d1777fc90
Givanaldo Rocha <givanaldo@gmail.com>
2019-08-13 16:23:32 -0300

    Atualização Matrizes.java

--- ConceitosIniciais/src/iniciais/Matrizes.java
+++ ConceitosIniciais/src/iniciais/Matrizes.java
@@ -6,26 +6,34 @@ public class Matrizes {
     
     public static void main(String[] args) {
         
-        int linhas = 2, colunas = 4;
+        int linhas = 5; // alunos
+        int colunas = 4; // notas
         float notas[][] = new float[linhas][colunas];
+        float media[] = new float[linhas];
+        String situacao[] = new String[linhas];
         
-        Scanner entrada = new Scanner(System.in);
+        Scanner teclado = new Scanner(System.in);
         
         for (int i=0; i<linhas; i++) {
             System.out.println("Aluno " + (i+1) + ":");
             for (int j=0; j<colunas; j++) {
-                System.out.println("- nota " + (j+1) + ": ");
-                notas[i][j] = entrada.nextFloat();
+                System.out.print("- nota " + (j+1) + ": ");
+                notas[i][j] = teclado.nextFloat();
+                media[i] += notas[i][j];
             }
+            media[i] /= 4;
+            if (media[i] >= 6.0) situacao[i] = "Aprovado";
+            else if (media[i] >= 3.0) situacao[i] = "Recuperação";
+            else situacao[i] = "Reprovado";
         }
+                    
         
-        System.out.println("\nNOTAS DIGITADAS: ");
+        System.out.println("\nRESULTADOS: ");
         for (int i=0; i<linhas; i++) {
             System.out.print("Aluno " + (i+1) + ": ");
             for (int j=0; j<colunas; j++) 
                 System.out.print(notas[i][j] + " ");
-            System.out.println("");  
+            System.out.println(" -- Média = " + media[i] + " -- Situação: " + situacao[i]);  
         }
     }
-    
 }
