f49af70899cacdc88bf62301f634b7dcd8c602bd
givanaldo <givanaldo@gmail.com>
2021-07-08 07:50:05 -0300

    ajuste no teste simples

--- 03-Ordenação/src/algoritmos/TesteSimplesOrdenacao.java
+++ 03-Ordenação/src/algoritmos/TesteSimplesOrdenacao.java
@@ -5,22 +5,20 @@ public class TesteSimplesOrdenacao {
     public static void main(String[] args) {
 
         int[] vetor1 = {3, 6, 2, 9, 8, 7, 4};
+        int[] vetor2 = vetor1.clone();
+        int[] vetor3 = vetor1.clone();
         
         Sort.bubblesort(vetor1);
         System.out.println("BubbleSort");
         for (int i=0; i<vetor1.length; i++)
             System.out.printf("%d ", vetor1[i]);
         System.out.println("\n");
-
-        int[] vetor2 = {3, 6, 2, 9, 8, 7, 4};
-        
+       
         Sort.selectionsort(vetor2);
         System.out.println("SelectionSort");
         for (int i=0; i<vetor2.length; i++)
             System.out.printf("%d ", vetor2[i]);
         System.out.println("\n");
-
-        int[] vetor3 = {3, 6, 2, 9, 8, 7, 4};
         
         Sort.insertionsort(vetor3);
         System.out.println("InsertionSort");
